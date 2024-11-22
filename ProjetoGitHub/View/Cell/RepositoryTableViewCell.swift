e7628248f57886491ad11169001215629bf31bc3
gibonifacio <gibonifacio3@gmail.com>
2024-11-21 21:50:25 -0300

    colocar cores

--- ProjetoGitHub/View/Cell/RepositoryTableViewCell.swift
+++ ProjetoGitHub/View/Cell/RepositoryTableViewCell.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+import Kingfisher
+
 class RepositoryTableViewCell: UITableViewCell {
     
     static let identifier = "RepositoryCell"
@@ -43,31 +45,39 @@ class RepositoryTableViewCell: UITableViewCell {
     let forksLabel: UILabel = {
         let forks = UILabel()
         forks.translatesAutoresizingMaskIntoConstraints = false
+        forks.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        forks.textColor = UIColor(named: "light-purple")
         return forks
     }()
     
     let starsLabel: UILabel = {
         let stars = UILabel()
         stars.translatesAutoresizingMaskIntoConstraints = false
+        stars.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        stars.textColor = UIColor(named: "light-purple")
+
         return stars
     }()
     
     let image: UIImageView = {
         let image = UIImageView()
         image.image = UIImage(systemName: "heart")
+        image.translatesAutoresizingMaskIntoConstraints = false
         return image
     }()
     
     let usernameLabel: UILabel = {
         let username = UILabel()
         username.translatesAutoresizingMaskIntoConstraints = false
+        username.font = UIFont.systemFont(ofSize: 14, weight: .semibold)
+        username.textColor = UIColor(named: "lilac")
         return username
     }()
     
     let forkImage: UIImageView = {
         let image = UIImageView()
         image.image = UIImage(systemName: "tuningfork")
-        image.tintColor = UIColor(named: "dark-purple")
+        image.tintColor = UIColor(named: "light-purple")
         image.translatesAutoresizingMaskIntoConstraints = false
         return image
     }()
@@ -75,7 +85,7 @@ class RepositoryTableViewCell: UITableViewCell {
     let starsImage: UIImageView = {
         let image = UIImageView()
         image.image = UIImage(systemName: "star.fill")
-        image.tintColor = UIColor(named: "dark-purple")
+        image.tintColor = UIColor(named: "light-purple")
         image.translatesAutoresizingMaskIntoConstraints = false
         return image
     }()
@@ -87,6 +97,11 @@ class RepositoryTableViewCell: UITableViewCell {
         self.bodyLabel.text = description
         self.starsLabel.text = String(stargazersCount)
         self.forksLabel.text = String(forksCount)
+        if let url = URL(string: avatarUrl) {
+            self.image.kf.setImage(with: url)
+        } else {
+            print("URL inválida: \(avatarUrl)")
+        }
         setElements()
     }
     
@@ -101,14 +116,12 @@ class RepositoryTableViewCell: UITableViewCell {
             view.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 12)
         ])
 
-        // Adicionar o título
         view.addSubview(titleLabel)
         NSLayoutConstraint.activate([
             titleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 15),
             titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20)
         ])
 
-        // Adicionar o corpo da descrição
         view.addSubview(bodyLabel)
         NSLayoutConstraint.activate([
             bodyLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8),
@@ -116,9 +129,7 @@ class RepositoryTableViewCell: UITableViewCell {
             bodyLabel.widthAnchor.constraint(equalToConstant: 220)
         ])
 
-        // Adicionar imagem de forks e label correspondente
         view.addSubview(forkImage)
-        forkImage.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             forkImage.topAnchor.constraint(equalTo: bodyLabel.bottomAnchor, constant: 10),
             forkImage.leadingAnchor.constraint(equalTo: bodyLabel.leadingAnchor),
@@ -132,7 +143,6 @@ class RepositoryTableViewCell: UITableViewCell {
             forksLabel.leadingAnchor.constraint(equalTo: forkImage.trailingAnchor, constant: 8)
         ])
 
-        // Adicionar imagem de stars e label correspondente
         view.addSubview(starsImage)
         starsImage.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
@@ -148,19 +158,18 @@ class RepositoryTableViewCell: UITableViewCell {
             starsLabel.leadingAnchor.constraint(equalTo: starsImage.trailingAnchor, constant: 8)
         ])
 
-        // Adicionar imagem e username
         view.addSubview(image)
         NSLayoutConstraint.activate([
-            image.topAnchor.constraint(equalTo: view.topAnchor, constant: 20),
+            image.topAnchor.constraint(equalTo: view.topAnchor, constant: 40),
             image.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-            image.heightAnchor.constraint(equalToConstant: 30),
-            image.widthAnchor.constraint(equalToConstant: 30)
+            image.heightAnchor.constraint(equalToConstant: 50),
+            image.widthAnchor.constraint(equalToConstant: 50)
         ])
 
         view.addSubview(usernameLabel)
         NSLayoutConstraint.activate([
             usernameLabel.topAnchor.constraint(equalTo: image.bottomAnchor, constant: 8),
-            usernameLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+            usernameLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
         ])
 
     }
